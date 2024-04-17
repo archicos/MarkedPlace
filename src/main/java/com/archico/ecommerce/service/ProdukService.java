d232cf779b8284193361e35ab88e6a93ec153714
ifs21011-itdel <archicosemb@gmail.com>
2024-04-17 17:18:21 +0700

    feat add controller for Produk and Rest API for Produk

--- src/main/java/com/archico/ecommerce/service/ProdukService.java
+++ src/main/java/com/archico/ecommerce/service/ProdukService.java
@@ -3,16 +3,22 @@ package com.archico.ecommerce.service;
 import java.util.List;
 import java.util.UUID;
 
+import org.apache.coyote.BadRequestException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.util.StringUtils;
 
 import com.archico.ecommerce.entity.Produk;
 import com.archico.ecommerce.exception.ResourceNotFoundException;
+import com.archico.ecommerce.repository.KategoriRepository;
 import com.archico.ecommerce.repository.ProdukRepository;
 
 @Service
 public class ProdukService {
     
+    @Autowired
+    private KategoriRepository kategoriRepository;
+
     @Autowired
     private ProdukRepository produkRepository;
 
@@ -24,12 +30,48 @@ public class ProdukService {
         return produkRepository.findById(id).orElseThrow(() -> new ResourceNotFoundException("Produk dengan id " + id + " tidak ditemukan"));
     }
 
-    public Produk create(Produk produk){
+    public Produk create(Produk produk) throws BadRequestException{
+        if (!StringUtils.hasText(produk.getNama())) {
+            throw new BadRequestException("Nama produk tidak boleh kosong");
+        }
+
+        if (produk.getKategori() == null) {
+            throw new BadRequestException("Kategori tidak boleh kosong");
+        }
+
+        if (!StringUtils.hasText(produk.getKategori().getId())) {
+            throw new BadRequestException("Kategori ID tidak boleh kosong");
+        }
+
+        kategoriRepository.findById(produk.getKategori().getId())
+        .orElseThrow(() -> new BadRequestException(
+            "Kategori ID " + produk.getKategori().getId() + " tidak ditemukan dalam database"));
+
         produk.setId(UUID.randomUUID().toString());
         return produkRepository.save(produk);
     }
 
-    public Produk edit(Produk produk){
+    public Produk edit(Produk produk) throws BadRequestException{
+        if (StringUtils.hasText(produk.getId())) {
+            throw new BadRequestException("Produk ID harus diisi");
+        }
+
+        if (!StringUtils.hasText(produk.getNama())) {
+            throw new BadRequestException("Nama produk tidak boleh kosong");
+        }
+
+        if (produk.getKategori() == null) {
+            throw new BadRequestException("Kategori tidak boleh kosong");
+        }
+
+        if (!StringUtils.hasText(produk.getKategori().getId())) {
+            throw new BadRequestException("Kategori ID tidak boleh kosong");
+        }
+
+        kategoriRepository.findById(produk.getKategori().getId())
+                .orElseThrow(() -> new BadRequestException(
+                        "Kategori ID " + produk.getKategori().getId() + " tidak ditemukan dalam database"));
+
         return produkRepository.save(produk);
     }
 
